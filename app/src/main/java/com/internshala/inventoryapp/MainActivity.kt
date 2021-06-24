cca5bb6a0984448383a4574a2688472b94b9ba11
Pranav <patilpranav915@gmail.com>
2021-06-24 14:20:26 +0530

    Bottom navigation view issue resolved

--- app/src/main/java/com/internshala/inventoryapp/MainActivity.kt
+++ app/src/main/java/com/internshala/inventoryapp/MainActivity.kt
@@ -1,12 +1,12 @@
 package com.internshala.inventoryapp
 
 import android.os.Bundle
-import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.internshala.inventoryapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -31,5 +31,7 @@ class MainActivity : AppCompatActivity() {
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+
+
     }
 }
